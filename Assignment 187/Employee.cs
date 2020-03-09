609182b3a21f4075a3a824c00bc0e92afa593084
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-09 15:14:57 -0700

    Add project files.

---
+++ Assignment 187/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_187
+{
+    public class Employee<T>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public Employee(int id, string fn, string ln)
+        {
+            id = ID;
+            fn = FirstName;
+            ln = LastName;
+        }
+        public List<T> things { get; set; }
+    }
+}
---
+++ Assignment 187/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_187
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> emp1 = new Employee<string>(1, "Richard", "Quetives");
+            emp1.things = new List<string>() { "Richard", "Quetives", "was", "hired." };
+            emp1.things.ForEach(i => Console.Write("{0} ", i));
+            Console.ReadLine();
+
+            Employee<int> emp2 = new Employee<int>(2, "Anahiza", "Carrillo");
+            emp2.things = new List<int>() { 22, 35, 14, 47 };
+            emp2.things.ForEach(i => Console.Write("{0} \n", i));
+            Console.ReadLine();
+        }
+    }
+}
